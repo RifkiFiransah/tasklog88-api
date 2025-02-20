86b17ff3978ae95c5c9ee5867685434ea2b52855
rifkifiransah <muhammadrifkifiransah@gmail.com>
2025-02-20 10:43:28 +0700

    feat: change update user by id without password and within password

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -8,6 +8,7 @@ router.get("/users", userController.GetAllUsers)
 router.get("/users/:id_user", userController.GetUserById)
 router.post("/users/", userController.AddUser)
 router.put("/users/:id_user", userController.UpdateUserById)
+router.put("/users/password/:id_user", userController.UpdateUserWithPassword)
 router.delete("/users/:id_user", userController.DeleteUserById)
 
 export default router
